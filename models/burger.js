3dd7290bdc35e72a55689146d41e1690d4150e88
zahra falah <sepidehfallah2@gmail.com>
2018-05-04 20:03:35 -0700

    models setup

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+// import orm object
+var orm = require ('../config/orm.js');
+
+var burger = {
+    
+    selectAll: function(cb) {
+      orm.selectAll('burgers', function(res) {
+        cb(res);
+      });
+    },
+  
+   
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne('burgers', cols, vals, function(res) {
+        cb(res);
+      });
+    },
+  
+  
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne('burgers', objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller
+  module.exports = burger;
+
