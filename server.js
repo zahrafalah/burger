76f58cc0aa38f9f05b8fbc27d42f16a858fec718
zahra falah <sepidehfallah2@gmail.com>
2018-05-08 17:01:46 -0700

    finall

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
+var methodOverride = require('method-override')
 // var path = require("path");
 
 
@@ -15,6 +16,9 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static(process.cwd() + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 // Set Handlebars as the view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
